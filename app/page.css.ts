73d137a6732c485ff9892981c3094c58dd97985f
dohee <seong0420hei@gmail.com>
2024-09-19 22:41:14 +0900

    init @vanilla-extract

---
+++ app/page.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+export const main = style({
+  width: '100%',
+  minHeight: '100vh',
+  background: 'radial-gradient(circle, rgb(17, 17, 17) 0%, rgb(0, 0, 0) 65%)'
+})
